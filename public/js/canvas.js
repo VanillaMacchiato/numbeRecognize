ea800b878bf70212826e95b3a2cca9650466528b
vito <vitosgh@gmail.com>
2022-08-09 16:14:25 +0700

    First working program

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,60 +1,100 @@
-const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
-const ctx = canvas.getContext('2d');
-
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-
-const boundary = canvas.getBoundingClientRect();
-
-canvas.width = 400;
-canvas.height = 400;
-
-let isPainting = false;
-let lineWidth = 10;
-let startX;
-let startY;
-
-toolbar.addEventListener('click', (e) => {
-  if (e.target.id === 'clear') {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
+let model;
+tf.loadLayersModel('model/model.json').then((md) => {
+  model = md;
 });
 
-toolbar.addEventListener('change', (e) => {
-  if (e.target.id === 'lineWidth') {
-    lineWidth = e.target.value;
-  }
-});
+const useDrawingCanvas = () => {
+  const canvas = document.getElementById('drawing-board');
+  const toolbar = document.getElementById('toolbar');
+  const ctx = canvas.getContext('2d');
 
-const draw = (e) => {
-  if (!isPainting) {
-    return;
-  }
+  canvas.width = 400;
+  canvas.height = 400;
 
-  ctx.lineWidth = lineWidth;
-  ctx.lineCap = 'round';
+  let isPainting = false;
+  let lineWidth = 10;
 
-  const boundary = canvas.getBoundingClientRect();
-  ctx.lineTo(e.clientX - boundary.left, e.clientY - boundary.top);
-  ctx.stroke();
+  toolbar.addEventListener('click', (e) => {
+    if (e.target.id === 'clear') {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  });
+
+  toolbar.addEventListener('change', (e) => {
+    if (e.target.id === 'lineWidth') {
+      lineWidth = e.target.value;
+    }
+  });
+
+  const draw = (e) => {
+    if (!isPainting) {
+      return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    const boundary = canvas.getBoundingClientRect();
+    ctx.lineTo(e.clientX - boundary.left, e.clientY - boundary.top);
+    ctx.stroke();
+  };
+
+  canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+  });
+
+  canvas.addEventListener('mousemove', draw);
+
+  canvas.addEventListener('mouseup', (e) => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+
+    const img = canvas.toDataURL('image/png');
+
+    const resizeCanvas = document.getElementById('dummy-canvas');
+    const resizeCtx = resizeCanvas.getContext('2d');
+
+    resizeCanvas.width = 28;
+    resizeCanvas.height = 28;
+
+    let resizeImage = new Image();
+
+    resizeImage.onload = () => {
+      resizeCtx.drawImage(resizeImage, 0, 0, resizeCanvas.width, resizeCanvas.height);
+
+      dummyImage = document.getElementById('dummy-img');
+      dummyImage.src = resizeCanvas.toDataURL('image/png');
+
+      // process and predict the image
+      const arr = resizeCtx.getImageData(0, 0, resizeCanvas.width, resizeCanvas.height);
+      const mono = convertToMono(arr.data, resizeCanvas.width, resizeCanvas.height);
+      const imageTensor = tf.tensor4d(mono, [1, resizeCanvas.width, resizeCanvas.height, 1]);
+
+      const prediction = model.predict(imageTensor);
+      
+      // send to the document
+      document.getElementById('result-number').innerText = prediction.argMax(1).dataSync()[0];
+    };
+
+    resizeImage.src = img;
+  });
 };
 
-canvas.addEventListener('mousedown', (e) => {
-  isPainting = true;
-  startX = e.clientX;
-  startY = e.clientY;
-});
+const convertToMono = (arr, width, height) => {
+  const result = new Float32Array(width * height);
 
-canvas.addEventListener('mouseup', (e) => {
-  isPainting = false;
-  ctx.stroke();
-  ctx.beginPath();
+  let idx;
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++) {
+      idx = j * 4 * width + i * 4;
+      result[j * width + i] = arr[idx + 3] / 255.0;
+    }
+  }
 
-  const img = canvas.toDataURL('image/png');
-  console.log(img.size)
+  return result;
+};
 
-  document.getElementById('result').src = img;
-});
-
-canvas.addEventListener('mousemove', draw);
+useDrawingCanvas();
